859b5c65a6bc44131a21fc26ea720eea5db1df48
Maria Deslis <mdeslis@gmail.com>
2018-04-10 23:31:22 -0600

    added images directory with png character images

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-const http = require('http');
+var http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+//create a server object:
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(req.url); //write a response to the client
+  // holds part of the url that comes after the domain name
+  res.end(); //end the response
+}).listen(3000); //the server object listens on port 8080
